111a068650831c33b08d8cff9c47bc710021e669
Simon Prickett <simon@crudworks.org>
2024-02-04 13:15:18 +0000

    Work in progress.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 # TODO make this work with other Unicorns?
 from cosmic import CosmicUnicorn as Unicorn
@@ -8,58 +9,155 @@ graphics = PicoGraphics(DISPLAY)
 
 DISPLAY_WIDTH = unicorn.WIDTH
 DISPLAY_HEIGHT = unicorn.HEIGHT
+DISPLAY_MID_POINT = DISPLAY_WIDTH // 2
 
-DAY_COLOUR = "TODO"
-DAY_BALL_COLOUR = "TODO"
-NIGHT_COLOUR = "TODO"
-NIGHT_BALL_COLOUR = "TODO"
+DAY_COLOUR = (209, 245, 66)
+NIGHT_COLOUR = (69, 66, 245)
+DAY_BALL_COLOUR = NIGHT_COLOUR
+NIGHT_BALL_COLOUR = DAY_COLOUR
 
-SQUARE_SIZE = "TODO"
-BALL_SIZE = "TODO"
+DAY_PEN = graphics.create_pen(*DAY_COLOUR)
+NIGHT_PEN = graphics.create_pen(*NIGHT_COLOUR)
 
-SQUARES_X = "TODO"  # How many squares wide
-SQUARES_Y = "TODO"  # How many squares tall
+SQUARE_SIZE = 2
+BALL_SIZE = 2
 
 LOOP_SLEEP_TIME = 0.1
 
-def draw_squares():
-    # TODO
+class Ball:
+    def __init__(self, x, y, pen_colour, background_pen_colour):
+        self.x = x
+        self.y = y
+        self.w = BALL_SIZE
+        self.h = BALL_SIZE
+        self.pen = graphics.create_pen(*pen_colour)
+        self.background_pen = graphics.create_pen(*background_pen_colour)
+        self.dx = random.choice([-1, 1])
+        self.dy = random.choice([-1, 1])
+        
+    # TODO consider making this just the initial draw
+    # and have next_position do everything else.
+    # Or can we just remove this completely?
+    def draw(self):
+        graphics.set_pen(self.pen)
+        graphics.rectangle(self.x, self.y, self.h, self.w)
+        
+    def erase(self):
+        graphics.set_pen(self.background_pen)
+        graphics.rectangle(self.x, self.y, self.h, self.w)
+        
+    def next_position(self):        
+        next_x = self.x + self.dx
+        next_y = self.y + self.dy
+        
+        if next_x < 0:
+            next_x = 0
+            
+        if next_y < 0:
+            next_y = 0
+            
+        # TODO replace -1 here with a calculation.
+        if next_x + (self.w - 1) == DISPLAY_WIDTH:
+            next_x = self.x
 
+        # TODO replace -1 here with a calculation.
+        if next_y + (self.h - 1) == DISPLAY_HEIGHT:
+            next_y = self.y
+            
+        # TODO adjust dx, dy... need to work out a
+        # transition table here.
+        
+        self.x = next_x
+        self.y = next_y
+            
 
-def draw_ball(x, y, colour):
-    # TODO
-    pass
+def init_squares():
+    graphics.clear()
+    
+    # Draw the left hand half of the screen using the day colour
+    # and the right hand half using the night colour.
+    for x in range(0, DISPLAY_HEIGHT, SQUARE_SIZE):
+        for y in range(0, DISPLAY_WIDTH, SQUARE_SIZE):
+            graphics.set_pen(DAY_PEN if x < DISPLAY_MID_POINT else NIGHT_PEN)
+            graphics.rectangle(x, y, SQUARE_SIZE, SQUARE_SIZE)
+        
+    unicorn.update(graphics)
+    
 
+def draw_balls():
+    day_ball.draw()
+    night_ball.draw()
+    
 
+def erase_balls():
+    day_ball.erase()
+    night_ball.erase()
+    
+    
+def update_ball_positions():
+    day_ball.next_position()
+    night_ball.next_position()
+    
 def check_collision(x, y, dx, dy):
     # TODO
     # TODO acc collision sound, optional?
-    pass
+    return "TODO"
 
 
 def draw_frame():
     # TODO draw a single frame.
+    return "TODO"
 
 
 # Set initial LED brightness.
 # TODO do we need to track this in a variable?
-unicorn.set_brightness(0.5)
-
+unicorn.set_brightness(0.2)
 
 frame_counter = 0
+
+# Draw the night/day start point.
+init_squares()
+
+# The day ball must start on the left half of the display,
+# with the night ball on the right.
+day_ball = Ball(
+    random.randrange(0, DISPLAY_MID_POINT, BALL_SIZE),
+    random.randrange(0, DISPLAY_HEIGHT, BALL_SIZE),
+    DAY_BALL_COLOUR,
+    NIGHT_BALL_COLOUR
+)
+
+night_ball = Ball(
+    random.randrange(DISPLAY_MID_POINT, DISPLAY_WIDTH, BALL_SIZE),
+    random.randrange(0, DISPLAY_HEIGHT, BALL_SIZE),
+    NIGHT_BALL_COLOUR,
+    DAY_BALL_COLOUR
+)
+                
+draw_balls()
+unicorn.update(graphics)
 
 while True:
     # Update LEDs roughly 60 times per second.
     # TODO maths
+    # TODO maybe allow colour cycling.
+    # TODO maybe allow frame rate changes.
     
-    if unicorn.is_pressed(CosmicUnicorn.SWITCH_BRIGHTNESS_UP):
+    erase_balls()
+    update_ball_positions()
+    draw_balls()
+    unicorn.update(graphics)
+    
+    if unicorn.is_pressed(Unicorn.SWITCH_BRIGHTNESS_UP):
         # TODO
         pass
 
-    if unicorn.is_pressed(CosmicUnicorn.SWITCH_BRIGHTNESS_DOWN):
+    if unicorn.is_pressed(Unicorn.SWITCH_BRIGHTNESS_DOWN):
         # TODO
         pass
     
     # TODO check for volume button presses if sound code is added.
   
-    time.sleep(LOOP_SLEEP_TIME)
+    # TODO put this back in when we've got movement down.
+    #time.sleep(LOOP_SLEEP_TIME)
+    time.sleep(1)
